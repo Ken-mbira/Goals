87993c1cf02249b221081398ee9b20e1de10399e
ken <ken.mbira@student.moringaschool.com>
2021-08-18 05:07:52 +0300

    add a line strikethrough onclick and remove it on double-click

--- src/app/strikethrough.directive.ts
+++ src/app/strikethrough.directive.ts
@@ -1,10 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appStrikethrough]'
 })
 export class StrikethroughDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) { }
 
+  @HostListener("click") onClicks(){
+  this.textdeco("line-through")
+  }
+  
+  @HostListener("dblclick") onDoubleClicks(){
+  this.textdeco("None")
+  }
+
+  private textdeco(action:string){
+    this.elem.nativeElement.style.textDecoration=action;
+  }
 }
