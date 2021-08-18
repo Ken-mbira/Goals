a70e7cf6811777936f2490aced8fd4b1a9584860
ken <ken.mbira@student.moringaschool.com>
2021-08-18 15:49:24 +0300

    add two-way data binding to show data as it is typed in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { GoalComponent } from './goal/goal.component';
-import { Goal } from 'src/app/goal';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +22,8 @@ import { GoalFormComponent } from './goal-form/goal-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
