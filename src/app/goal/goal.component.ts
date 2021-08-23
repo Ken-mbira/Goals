57c482e8058d4889c96f6df3525511f409bbe7f1
ken <ken.mbira@student.moringaschool.com>
2021-08-23 11:48:38 +0300

    add a service to alert when a goal is deleted

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Goal } from '../goal';
 import { GoalService } from '../goal-service/goal.service';
+import { AlertService } from '../alert-service/alert.service';
 
 @Component({
   selector: 'app-goal',
@@ -10,16 +11,18 @@ import { GoalService } from '../goal-service/goal.service';
 export class GoalComponent implements OnInit {
   title = "Goals"
   goals: Goal[];
-  constructor(goalService: GoalService) {
-    this.goals = goalService.getGoals();
-  }
+  alertService : AlertService;
   
   toggleDetails(index){
     this.goals[index].showDescription = !this.goals[index].showDescription;
   };
   completeGoal(isComplete, index){
     if(isComplete){
-      this.goals.splice(index,1);
+      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
+      if (toDelete){
+        this.goals.splice(index,1);
+        this.alertService.alertMe("The goal has been deleted!")
+      }
     }
   };
 
@@ -28,6 +31,11 @@ export class GoalComponent implements OnInit {
     goal.id = goalLength + 1;
     goal.completeDate = new Date(goal.completeDate);
     this.goals.push(goal)
+  }
+
+  constructor(goalService: GoalService, alertService : AlertService) {
+    this.goals = goalService.getGoals();
+    this.alertService = alertService;
   }
   
 
